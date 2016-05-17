be4b9d59dd5e6f457c1e2ea237aebb275d8f8900
Liam R. Howlett <Liam.Howlett@WindRiver.com>
2016-05-17 13:51:08 -0400

    anspassd.h, anspass.h, anspass-ctrl.h, anspass-lib.h: Add lgpl license information.
    Added lgpl v2.1 as per Jason's suggestion.
    
    Signed-off-by: Liam R. Howlett <Liam.Howlett@WindRiver.com>

--- anspassd.h
+++ anspassd.h
@@ -1,3 +1,26 @@
+/*
+ * anspassd: Daemon to answer anspass queries and anspass-ctrl requests.
+ *
+ * Copyright (C) 2016 Wind River Systems, Inc.
+ * Written by Liam R. Howlett <Liam.Howlett@Windriver.com>
+ *
+ * This file is part of anspass
+ *
+ * anspass is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * anspass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 #include <signal.h>
 #include <string.h>
 #include <stdio.h>
